7309f8059dbffecc4fefb4548b661b2b946000ba
Rohan Pandit <pandit.rohan@gmail.com>
2015-03-31 09:36:16 -0700

    fixed firebase errors

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
+var data
 var firebaseData = new Firebase("https://scheduleanalyzer.firebaseio.com/data");
-var data;
-
 firebaseData.once('value', function(snap) {
     data = snap.val();
 });
 
-firebaseData.on("value", function(snapshot) {
-      data = snapshot.val();
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-});
 
 $('#submitAdvice').click( 
     function(){
@@ -18,36 +12,56 @@ $('#submitAdvice').click(
         hours = parseFloat($('#hours').val());
         difficulty = parseFloat($('#difficulty').val());
         enter(className, teacher, hours, difficulty);
-        $('#className').val('');
-        $('#teacherName').val('');
-        $('#hours').val('');
-        $('#difficulty').val('');
+
     }
 );
 
 function enter(c, t, hours, difficulty){ //class, teacher, hours, difficulity
-    console.log(data)
-    console.log(c+t)
-	var num = data[c][t]['numEntries']
-    data[c][t]['hours'] = (data[c][t]['hours']*num + hours)/(num + 1)
-	data[c][t]['difficulty'] = parseFloat((data[c][t]['difficulty']*num + difficulty)/(num + 1))  
-    
-	data[c][t]['hoursVariance'] = (parseInt(data[c][t]['hoursVariance'])*num + Math.pow(hours - data[c][t]['hours'], 2))/(num + 1)
-                                   
-	data[c][t]['difficultyVariance'] = (parseInt(data[c][t]['difficultyVariance'])*num + Math.pow(difficulty - data[c][t]['difficulty'], 2))/(num + 1) 
+    if(data[c][t]){
+        var datact
+        var firebase = new Firebase("https://scheduleanalyzer.firebaseio.com/data/"+c.replace(' ', '%20')+'/' + t)
+        firebase.once('value', function(snap) {
+            datact = snap.val();
+            var num = datact['numEntries']
 
-	data[c][t]['numEntries'] += 1
-    
-    firebaseData.update(data, function(){
-            $('#rightContainer').append("<div class='pure-g'> <div class='pure-u-4-5'> <center> <span id='successText'> Thank you! Your response has been recorded </span> </center></div> <div class='pure-u-1-5'> </div> </div> ");
-            setTimeout(removeSuccess, 3000);
-            console.log("success!");
-        }
-    );
+            datact['hours'] = (datact['hours']*num + hours)/(num + 1)
+            datact['difficulty'] = parseFloat((datact['difficulty']*num + difficulty)/(num + 1))
+            datact['hoursVariance'] = (parseInt(datact['hoursVariance'])*num + Math.pow(hours - datact['hours'], 2))/(num + 1)
+            datact['difficultyVariance'] = (parseInt(datact['difficultyVariance'])*num + Math.pow(difficulty - datact['difficulty'], 2))/(num + 1)
+            datact['numEntries'] += 1
+            console.log("here")
+            firebase.update(datact, function(o){
+                if(o){
+                    console.log("failure " + o);
+                    addFailure()                    
+                    setTimeout(removeMessage, 3000);
+                } else {
+                    addSuccess()
+                    setTimeout(removeMessage, 3000);
+                    console.log("success!");
+                    $('#className').val('');
+                    $('#teacherName').val('');
+                    $('#hours').val('');
+                    $('#difficulty').val('');
+                }
+            })
+        });   
+    }  else {
+        $('#rightContainer').append("<div class='pure-g'> <div class='pure-u-4-5'> <center> <span class='message'> Class/Teacher not found. Be sure to use the autocomplete suggestions. </span> </center></div> <div class='pure-u-1-5'> </div> </div> ");
+    }
 }
 
-function removeSuccess(){
-    $('#successText').fadeOut();
+function addSuccess(){
+            $('#rightContainer').append("<div class='pure-g'> <div class='pure-u-4-5'> <center> <span class='message'> Thank you! Your response has been recorded </span> </center></div> <div class='pure-u-1-5'> </div> </div> ");
+
+}
+
+function addFailure(){
+    $('#rightContainer').append("<div class='pure-g'> <div class='pure-u-4-5'> <center> <span class='message'> Thank you! Your response has been recorded </span> </center></div> <div class='pure-u-1-5'> </div> </div> ");
+}
+
+function removeMessage(){
+    $('.message').fadeOut();
 }
 
 
